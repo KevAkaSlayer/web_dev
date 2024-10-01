a01db1b62ff5d3584df1a57c5fe4b8cf580e8277
KevAkaSlayer <pukman41@gmail.com>
2024-10-01 18:37:34 +0600

    exploring api

---
+++ Api/post.js
@@ -0,0 +1,23 @@
+function loadPost() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(response => response.json())
+    .then(data => displayPosts(data))
+}
+
+
+
+function displayPosts(posts) {
+    const postDiv = document.getElementById('post-container');
+    for(const post of posts) {
+        const div = document.createElement('div');
+        div.classList.add('post');
+        div.innerHTML = `
+            <h3>${post.title}</h3>
+            <p>${post.body}</p>
+        `;
+        postDiv.appendChild(div);
+    }
+}
+
+
+loadPost();
