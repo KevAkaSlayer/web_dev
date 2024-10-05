90c78b31348bd2896064a6c9cd997248074a3b8d
KevAkaSlayer <pukman41@gmail.com>
2024-10-05 23:51:16 +0600

    loadvideos added

--- ph-tube/app.js
+++ ph-tube/app.js
@@ -15,8 +15,32 @@ const displayCat = (categories) => {
 
         catContainer.appendChild(button);
     });
+}
 
+const loadVideos = () => {
+    fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
+    .then(res => res.json())
+    .then(data => displayVideos(data.videos))
+    .then(err => console.log(err))
 
 }
+
+
+const displayVideos = (videos) => {
+    const videoContainer = document.getElementById('videos');
+    videos.forEch((video) => {
+        const div = document.createElement('div');
+        div.classList = "video";
+        div.innerHTML = `
+        <img src="${video.thumbnail}" alt="thumbnail">
+        <h3>${video.title}</h3>
+        <p>${video.description}</p>
+        `
+        videoContainer.appendChild(div);
+    })
+}
+
+
           
 loadCategories();
+loadVideos();
