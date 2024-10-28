aefc29fad2b21243f6d92e111fb610522e5dc94a
KevAkaSlayer <pukman41@gmail.com>
2024-10-28 23:40:19 +0600

    cook button functionalities added

--- chef-table/src/App.jsx
+++ chef-table/src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import Header from "./components/Header"
 import Hero from "./components/Hero"
 import Recipe from "./components/Recipe"
@@ -6,13 +7,17 @@ import SideBar from "./components/SideBar"
 
 
 function App() {
-  // const [recipeQ, setRecipeQ] = useState([])
-
+  const [recipeQ, setRecipeQ] = useState([])
 
   const addRecipeQ = (card) => {
-    console.log(card);
+    const isExist = recipeQ.find((item) => item.recipe_id === card.recipe_id)
+    if (!isExist) {
+      setRecipeQ([...recipeQ, card])
+    }
+    else {
+      alert("Already Added")
+    }
   }
-
   return (
     <>
       <div className="w-11/12 mx-auto">
@@ -24,7 +29,7 @@ function App() {
         {/* recipe card section */}
         <section className="flex flex-col md:flex-row gap-6 mt-10">
           <RecipeCard addRecipeQ={addRecipeQ}></RecipeCard>
-          <SideBar></SideBar>
+          <SideBar recipeQ={recipeQ}></SideBar>
         </section>
 
       </div>
--- chef-table/src/components/SideBar.jsx
+++ chef-table/src/components/SideBar.jsx
@@ -1,10 +1,15 @@
+import PropType from 'prop-types';
 
-const SideBar = () => {
+const SideBar = ({recipeQ}) => {
     return (
         <div className="md:w-1/3">
-            
+            <h3>Want To Cook : {recipeQ.length}</h3>
         </div>
     );
 };
+
+SideBar.propTypes = {
+    recipeQ: PropType.array
+}
 
 export default SideBar;
