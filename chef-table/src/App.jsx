f0568e1d3a641a3963f6dada0753523cea543c3d
KevAkaSlayer <pukman41@gmail.com>
2024-10-29 19:25:06 +0600

    chef-table done

--- chef-table/src/App.jsx
+++ chef-table/src/App.jsx
@@ -8,6 +8,9 @@ import SideBar from "./components/SideBar"
 
 function App() {
   const [recipeQ, setRecipeQ] = useState([])
+  const [preparedRecipe, setPreparedRecipe] = useState([])
+  const [totalTime, setTotalTime] = useState(0)
+  const [totalCalories, setTotalCalories] = useState(0)
 
   const addRecipeQ = (card) => {
     const isExist = recipeQ.find((item) => item.recipe_id === card.recipe_id)
@@ -17,6 +20,17 @@ function App() {
     else {
       alert("Already Added")
     }
+  }
+
+  const handleRemove = (id) => {
+    const newRecipeQ = recipeQ.find((recipe) => recipe.recipe_id === id)
+    setRecipeQ(recipeQ.filter((recipe) => recipe.recipe_id !== id))
+    setPreparedRecipe([...preparedRecipe,newRecipeQ])
+  }
+
+  const calculateTotalTimeAndCalories = (time,calories) => {
+    setTotalTime(totalTime + time)
+    setTotalCalories(totalCalories + calories)
   }
   return (
     <>
@@ -29,7 +43,7 @@ function App() {
         {/* recipe card section */}
         <section className="flex flex-col md:flex-row gap-6 mt-10">
           <RecipeCard addRecipeQ={addRecipeQ}></RecipeCard>
-          <SideBar recipeQ={recipeQ}></SideBar>
+          <SideBar handleRemove={handleRemove} recipeQ={recipeQ} preparedRecipe={preparedRecipe} calculateTotalTimeAndCalories = {calculateTotalTimeAndCalories} totalTime={totalTime} totalCalories={totalCalories}></SideBar>
         </section>
 
       </div>
--- chef-table/src/components/SideBar.jsx
+++ chef-table/src/components/SideBar.jsx
@@ -1,15 +1,79 @@
 import PropType from 'prop-types';
 
-const SideBar = ({recipeQ}) => {
+const SideBar = ({recipeQ,handleRemove,preparedRecipe,calculateTotalTimeAndCalories,totalTime,totalCalories}) => {
     return (
-        <div className="md:w-1/3">
-            <h3>Want To Cook : {recipeQ.length}</h3>
+        <div className="md:w-1/3 border-2 rounded-2xl text-gray-600 p-2 bg-base-100">
+            <div className="overflow-x-auto">
+                <h2 className='border-b-2 mx-auto font-semibold text-gray-800 text-center text-2xl'>Want To Cook: {recipeQ.length}</h2>
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                    <tr>
+                        <th></th>
+                        <th>Name</th>
+                        <th>Time</th>
+                        <th>Calories</th>
+                        <th></th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        {recipeQ.map((recipe, index) => (
+                            <tr key={index} className='hover'>
+                                <th>{index + 1}</th>
+                                <td>{recipe.recipe_name}</td>
+                                <td>{recipe.preparing_time}</td>
+                                <td>{recipe.calories}</td>
+                                <td>
+                                    <button onClick={() => {handleRemove(recipe.recipe_id)
+                                        calculateTotalTimeAndCalories(recipe.preparing_time,recipe.calories)}
+                                    } className="btn btn-sm bg-green-400 rounded-full">Preparing</button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+            <div className="overflow-x-auto mt-5">
+                <h2 className='border-b-2 mx-auto font-semibold text-gray-800 text-center text-2xl'>Currently Cooking: {preparedRecipe.length}</h2>
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                    <tr>
+                        <th></th>
+                        <th>Name</th>
+                        <th>Time</th>
+                        <th>Calories</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        {preparedRecipe.map((recipe, index) => (
+                            <tr key={index} className='hover'>
+                                <th>{index + 1}</th>
+                                <td>{recipe.recipe_name}</td>
+                                <td>{recipe.preparing_time}</td>
+                                <td>{recipe.calories}</td>
+                            </tr>
+                        ))}
+                            <tr className='border-none'>
+                                <th></th>
+                                <td></td>
+                                <td>Total Time = {totalTime}</td>
+                                <td>Total Calories = {totalCalories}</td>
+                            </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
 
 SideBar.propTypes = {
-    recipeQ: PropType.array
+    recipeQ: PropType.array,
+    handleRemove: PropType.func,
+    preparedRecipe: PropType.array,
+    calculateTotalTimeAndCalories: PropType.func,
+    totalTime: PropType.number,
+    totalCalories: PropType.number
 }
 
 export default SideBar;
